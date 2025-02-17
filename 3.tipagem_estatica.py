63a84c9bcfe4ec3aec20bd02fa76832831bfe76a
Melyssa-design <MELISSA.BRITO.22@HOTMAIL.COM>
2025-02-17 18:42:46 +0000

    salvando

---
+++ 3.tipagem_estatica.py
@@ -0,0 +1,15 @@
+import os
+os.system("clear")
+# definiçao de tipos obrigatôria.
+
+nome:str # Equivale ao tipo caracter
+idade:int # Equivale ao tipo inteiro
+altura: float # equivale ao tipo real 
+
+nome = "Marta"
+idade = 23
+altura = 1.65
+
+print (nome)
+print(idade)
+print(altura)
---
+++ 4.solicitando_dado.py
@@ -0,0 +1,16 @@
+import os
+os.system("clear")
+
+#solicitando dados.
+nome = input ("Digite seu nome: ")
+idade = int(input ("Digite sua idade: "))
+altura = float(input ("Digite sua altura: "))
+
+# print(nome)
+# print(idade)
+# print(altura)
+
+# formatação de variavel usando f e {}
+print(f"Nome: {nome}")
+print(f"idade: {idade}")
+print(f"altura: {altura}")
