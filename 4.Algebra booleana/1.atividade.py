512c8f510924d1b35357efaaa81e7400494cd5d8
Melyssa-design <MELISSA.BRITO.22@HOTMAIL.COM>
2025-02-26 20:07:37 +0000

    atividade

---
+++ 4.Algebra booleana/1.atividade.py
@@ -0,0 +1,14 @@
+import os
+os.system("clear")
+
+login = input("Digite seu login: ")
+senha = input("Digite sua senha: ")
+
+login_cadastrado = "honey"
+senha_cadastrada = "gato"
+
+
+if Login == ogin_cadastrado and senha == senha_cadastrada:
+   PRINT("Bem vindo")
+else:
+   print("Login ou senha inválidos!")
