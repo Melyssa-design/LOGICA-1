5936370aaf3db8c95719eed0610fbd23b0961b1a
Melyssa-design <MELISSA.BRITO.22@HOTMAIL.COM>
2025-06-11 19:04:15 +0000

    salv

--- javascript/3.Var.js
+++
@@ -1,4 +0,0 @@
-// VARIÁVEL
-var nome = "Marta"
-// CODIGO LEGADO
-console.log("Nome: ", nome)
--- javascript/EXERCICIOS/5.EX.js
+++
@@ -1,9 +0,0 @@
-// Crie um vetor com 3 notas, calcule a média aritmética.
-// Mostre as 3 notas e informe a média.
-
-const numero = [7,6,9]
-
-console.log("Exibindo notas")
-const soma = numero.reduce((soma, atual) => soma + atual, 0)
-console.log(numero)
-console.log(`A média é: ${soma / numero.length}`)
